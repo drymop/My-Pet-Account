1b2d9c8309340562370e7592ac9064aeb7142216
lmtuan <lmtuan98@gmail.com>
2019-01-20 13:29:45 -0500

    Start working on server

---
+++ account.py
@@ -0,0 +1,26 @@
+import requests
+
+API_KEY = 'd7d2263e1acab85c1f6020b064745864'
+DEFAULT_ACCOUNT_ID = '5c43b4ca322fa06b677943fc'
+
+ACCOUNT_URL = 'http://api.reimaginebanking.com/accounts/{id}'
+
+class Account:
+    def __init__(self, account_id):
+        self._id = account_id
+        self.acc_url = ACCOUNT_URL.format(id=self._id)
+
+    def _get_account_info(self):
+        data = requests.get(self.acc_url, params={'key': API_KEY}).json()
+        return data
+
+    def get_deposits(self):
+        data = requests.get(self.acc_url + '/deposits', params={'key': API_KEY}).json()
+        return data
+
+    def get_purchases(self):
+        data = requests.get(self.acc_url + '/purchases', params={'key': API_KEY}).json()
+        return data
+
+acc = Account(DEFAULT_ACCOUNT_ID)
+data = acc.get_purchases()
