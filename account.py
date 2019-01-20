33a902db97df6b27f07fda4e72235ab0eef6e2e1
lmtuan <lmtuan98@gmail.com>
2019-01-20 14:15:46 -0500

    Add method to account

--- account.py
+++ account.py
@@ -1,9 +1,13 @@
+from collections import namedtuple
 import requests
+from datetime import datetime
 
 API_KEY = 'd7d2263e1acab85c1f6020b064745864'
 DEFAULT_ACCOUNT_ID = '5c43b4ca322fa06b677943fc'
 
 ACCOUNT_URL = 'http://api.reimaginebanking.com/accounts/{id}'
+
+Delta = namedtuple('Delta', 'date ammount')
 
 class Account:
     def __init__(self, account_id):
@@ -20,7 +24,25 @@ class Account:
 
     def get_purchases(self):
         data = requests.get(self.acc_url + '/purchases', params={'key': API_KEY}).json()
-        return data
+        purchases = []
+        for x in data:
+            if x['status'] != 'cancelled':
+                date = self._str2date(x['purchase_date'])
+                ammount = x['amount']
+                purchases.append(Delta(date, ammount))
+        return purchases
+
+    def _str2date(self, date_str):
+        return datetime.strptime(date_str, '%Y-%m-%d') #yyyy-mm-dd
+
+
+
+
+
+
+
+
+
 
     def __str__(self):
         info = '\tname: {}\n\tif: {}\n\tbalance: {}'.format(self.name,
@@ -28,4 +50,4 @@ class Account:
                                                             self.money)
 
 acc = Account(DEFAULT_ACCOUNT_ID)
-data = acc.get_purchases()
+purs = acc.get_purchases()
