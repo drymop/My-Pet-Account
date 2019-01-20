95c806fd39c5129a7724c3e610030cdf6a6aa9dd
aalok.sathe <aalok.sathe@gmail.com>
2019-01-20 14:53:18 -0500

    add buncha methods

--- regress.py
+++ regress.py
@@ -6,3 +6,13 @@ class BankRegression(LinearRegression):
 
     def __init__(self):
         super().__init__()
+
+    def fit(self, *args, **kwargs):
+        super().fit(*args, **kwargs)
+        return self.coef_, self.intercept_
+
+    def get_slope(self):
+        return self.coef_
+
+    def get_intercept(self):
+        return self.intercept_
